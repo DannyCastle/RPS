858d475943aa50341787642a15d937d8c9af6abe
Daniel Castillo <danny.castle@hotmail.com>
2021-10-11 22:27:28 -0400

    added messages and html for UI

--- script.js
+++ script.js
@@ -1,114 +1,122 @@
 function computerPlay () {
-            let cpuChoice;
-            let randInt = Math.floor(Math.random() * 3);
-            if (randInt == 0) {
-                cpuChoice = 'rock';
-                return cpuChoice;
-            }
-            cpuChoice = (randInt > 1) ? cpuChoice = 'scissors' : cpuChoice = 'paper';
-            return cpuChoice;
-            }
+    let cpuChoice;
+    let randInt = Math.floor(Math.random() * 3);
+    if (randInt == 0) {
+        cpuChoice = 'rock';
+        return cpuChoice;
+    }
+    cpuChoice = (randInt > 1) ? cpuChoice = 'scissors' : cpuChoice = 'paper';
+    return cpuChoice;
+    }
 
-        function playerPrompt(){
-            let valid = false;
-            let playerInput;
-            do{
-            playerInput = prompt("Please enter your choice:");
-            playerInput = playerInput.toLowerCase();
+function playerPrompt(){
+    let valid = false;
+    let playerInput;
+    do{
+    playerInput = prompt("Please enter your choice:");
+    playerInput = playerInput.toLowerCase();
 
-            alert(`You chose ${playerInput}`);
-            if (
-                (playerInput === 'rock') ||
-                (playerInput === 'paper') ||
-                (playerInput === 'scissors')
-                ){
-                valid = true;
-            }
-            else {
-                alert('That is not a valid choice!');
-            }
-
-            }
-
-            while (!valid);
-
-
-            return playerInput;
-
-
-        }
-
-    function displayTally (playerTally, cpuTally){
-        alert(`The current score is ${playerTally} vs ${cpuTally}.`)
-        return;
+    alert(`You chose ${playerInput}`);
+    if (
+        (playerInput === 'rock') ||
+        (playerInput === 'paper') ||
+        (playerInput === 'scissors')
+        ){
+        valid = true;
+    }
+    else {
+        alert('That is not a valid choice!');
+    }
 
     }
 
-    function getwhoWon(playerTally, cpuTally){
-        alert(`The final score is ${playerTally} vs ${cpuTally}!`);
+    while (!valid);
 
-        if (playerTally > cpuTally){
-            alert('You won this round!');
-        }
-        else if (playerTally < cpuTally){
-            alert('Suck it! You lost to the computer.');
-        }
-        else {
-            alert('It was a tie. Boring.');
-        }
+
+    return playerInput;
+
+
+}
+
+function displayTally (playerTally, cpuTally){
+    const summaryMessage = document.querySelector('#summary');
+    summaryMessage.textContent = `The current score is ${playerTally} vs ${cpuTally}.`;
     return;
+
+}
+
+function getwhoWon(playerTally, cpuTally){
+    const summaryMessage = document.querySelector('#summary');
+    let winMessage = 'You are a true hero. Congrats!';
+    let lostMessage = 'Damn bro, you lost to a mindless machine.';
+    let drawMessage = 'BOOOOORING';
+    let finalMessage = '';
+
+
+
+    if (playerTally > cpuTally){
+        finalMessage = winMessage;
     }
+    else if (playerTally < cpuTally){
+        finalMessage = lostMessage;
+    }
+    else {
+        finalMessage = drawMessage;
+    }
+    summaryMessage.textContent =`FULL TIME: ${playerTally} vs ${cpuTally}! ${finalMessage}`;
+
+return;
+}
 
 
 
-    function game(){
-        let lostMessage = 'You lost this one!';
-        let winMessage = 'You win mofucka!';
-        let drawMessage = 'This one was a tie.';
+function game(){
+    let lostMessage = 'You lost this one!';
+    let winMessage = 'You win mofucka!';
+    let drawMessage = 'This one was a tie.';
 
-        let playerTally = 0;
-        let cpuTally = 0;
+    let playerTally = 0;
+    let cpuTally = 0;
 
 
 
-        for (let i = 1; i < 6 ; i++){
-            let playerPlay = playerPrompt();
-            let cpuPlay = computerPlay();
+    for (let i = 1; i < 6 ; i++){
+        let playerPlay = playerPrompt();
+        let cpuPlay = computerPlay();
 
-            alert(`Game #${i}!
-                Your choice: ${playerPlay}
-                CPU choice: ${cpuPlay}`);
+        alert(`Game #${i}!
+            Your choice: ${playerPlay}
+            CPU choice: ${cpuPlay}`);
 
-                if (playerPlay === cpuPlay ){
-                    alert(drawMessage);
+            if (playerPlay === cpuPlay ){
+                alert(drawMessage);
+                displayTally(playerTally, cpuTally);
+                }
+            else if (
+                (playerPlay === 'rock' && cpuPlay === 'scissors') ||
+                (playerPlay === 'scissors' && cpuPlay === 'paper') ||
+                (playerPlay === 'paper' && cpuPlay === 'rock')
+                )
+                {
+                    alert(winMessage);
+                    playerTally = ++playerTally;
                     displayTally(playerTally, cpuTally);
-                    }
-                else if (
-                    (playerPlay === 'rock' && cpuPlay === 'scissors') ||
-                    (playerPlay === 'scissors' && cpuPlay === 'paper') ||
-                    (playerPlay === 'paper' && cpuPlay === 'rock')
-                    )
-                    {
-                        alert(winMessage);
-                        playerTally = ++playerTally;
-                        displayTally(playerTally, cpuTally);
-                    }
-                else if (
-                    (cpuPlay === 'rock' && playerPlay === 'scissors') ||
-                    (cpuPlay === 'scissors' && playerPlay === 'paper') ||
-                    (cpuPlay === 'paper' && playerPlay === 'rock')
-                    )
-                    {
-                        alert(lostMessage);
-                        cpuTally = ++cpuTally;
-                        displayTally(playerTally, cpuTally);
-                    }
-
-            }
-        getwhoWon(playerTally, cpuTally);
-
+                }
+            else if (
+                (cpuPlay === 'rock' && playerPlay === 'scissors') ||
+                (cpuPlay === 'scissors' && playerPlay === 'paper') ||
+                (cpuPlay === 'paper' && playerPlay === 'rock')
+                )
+                {
+                    alert(lostMessage);
+                    cpuTally = ++cpuTally;
+                    displayTally(playerTally, cpuTally);
+                }
 
         }
+    getwhoWon(playerTally, cpuTally);
 
+
+}
 game();
 
